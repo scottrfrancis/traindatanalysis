65ee875a9517b98fd9541942564be323ca3c64cf
Scott Francis <scott.russell.francis@gmail.com>
2014-09-13 13:58:30 -0700

    HR index plot prototyped

---
+++ pace4HR.R
@@ -0,0 +1,27 @@
+# pace4HR.R
+#
+# Builds a chart of pace as function of Heart Rate
+#
+
+# f<- repo.files[420]
+# 
+# 
+# segs<- read.csv( f )
+# 
+
+segs<- data.frame()
+for (f in repo.files)
+{
+  s<- read.csv( f )
+  
+  segs<- rbind( segs, s )
+}
+
+segs.run<- segs[segs$sport=='run',]
+segs.run$pace.mph<- (segs.run$distance.m/1609.34)/(segs.run$duration.s/(60*60))
+segs.run.good<- segs.run[ (segs.run$pace.mph > 0) & (segs.run$pace.mph < 15) 
+                          & (segs.run$hr.bpm > 40) 
+                          & (segs.run$hr.bpm < 190), ]
+
+plot( x=segs.run.good$pace.mph, y=segs.run.good$hr.bpm )
+
