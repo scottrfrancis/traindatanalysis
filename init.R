d5696bb425c1f198afae249d2edb39c906aff86a
Scott Francis <scottrfrancis@mini.local>
2014-09-14 12:34:00 -0700

    converting to ggplot and fixing some subsets

--- init.R
+++ init.R
@@ -1,6 +1,7 @@
 
 # directory of TCX file repo
-dropbox.root<- "~/scofra01/Dropbox"
+#dropbox.root<- "~/scofra01/Dropbox"
+dropbox.root<- "~/Dropbox"
 repo.dir<- paste( dropbox.root, "Apps", "tapiriik", sep="/" )
 
 repo.files<- list.files( repo.dir, pattern="*.segments.csv", full.names=TRUE )
--- pace4HR.R
+++ pace4HR.R
@@ -2,6 +2,7 @@
 #
 # Builds a chart of pace as function of Heart Rate
 #
+require( 'ggplot2' )
 
 # f<- repo.files[420]
 # 
@@ -21,7 +22,11 @@ segs.run<- segs[segs$sport=='run',]
 segs.run$pace.mph<- (segs.run$distance.m/1609.34)/(segs.run$duration.s/(60*60))
 segs.run.good<- segs.run[ (segs.run$pace.mph > 0) & (segs.run$pace.mph < 15) 
                           & (segs.run$hr.bpm > 40) 
-                          & (segs.run$hr.bpm < 190), ]
+                          & (segs.run$hr.bpm < 190)
+                          & !is.na( segs.run$hr.bpm) & !is.na( segs.run$pace.mph), ]
 
-plot( x=segs.run.good$pace.mph, y=segs.run.good$hr.bpm )
+# plot( x=segs.run.good$pace.mph, y=segs.run.good$hr.bpm,
+#       pch='.')
 
+qplot( x=segs.run.good$pace.mph, y=segs.run.good$hr.bpm,
+       )
