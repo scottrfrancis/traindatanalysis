007e7c60d21583dc65155323ad33f7a6cf0cd783
Scott Francis <scott.russell.francis@gmail.com>
2014-10-08 17:34:00 -0700

    group samples by 6 week cycle and then regress within each cycle

--- pace4HR.R
+++ pace4HR.R
@@ -18,6 +18,7 @@ for (f in repo.files)
   segs<- rbind( segs, s )
 }
 
+
 segs.run<- segs[segs$sport=='run',]
 segs.run$pace.mph<- (segs.run$distance.m/1609.34)/(segs.run$duration.s/(60*60))
 segs.run.good<- segs.run[ (segs.run$pace.mph > 0) & (segs.run$pace.mph < 15) 
@@ -25,8 +26,33 @@ segs.run.good<- segs.run[ (segs.run$pace.mph > 0) & (segs.run$pace.mph < 15)
                           & (segs.run$hr.bpm < 190)
                           & !is.na( segs.run$hr.bpm) & !is.na( segs.run$pace.mph), ]
 
+segs.run.good$datemonth<- as.factor( format( as.Date( segs.run.good$time ), format="%Y%m" ) )
+
+segs.run.good$daysAgo<- as.integer( Sys.Date() - as.Date( segs.run.good$time ) )
+segs.run.good$cyclesAgo<- as.integer( segs.run.good$daysAgo/(6*7) )
+
+
+
 # plot( x=segs.run.good$pace.mph, y=segs.run.good$hr.bpm,
 #       pch='.')
 
-qplot( x=segs.run.good$pace.mph, y=segs.run.good$hr.bpm,
-       )
+#  qplot( x=segs.run.good$pace.mph, y=segs.run.good$hr.bpm,
+#        colour=factor( datemonth ) )
+
+
+model<- lm( hr.bpm ~ pace.mph + factor( cyclesAgo ), data=segs.run.good )
+grid<- with( segs.run.good, expand.grid( 
+    pace.mph = seq( min( pace.mph ), max( pace.mph ), length= 20 ),
+    cyclesAgo= levels( factor( cyclesAgo )) ) )
+grid$hr.bpm<- predict( model, newdata=grid )
+
+#qplot( pace.mph, hr.bpm, data=segs.run.good, color=factor( datemonth ) ) + geom_line( data=grid )
+qplot( pace.mph, hr.bpm, data=grid, geom="line", color=factor(cyclesAgo) )
+
+
+# p<- ggplot( segs.run.good, aes( pace.mph, hr.bpm ) )
+
+
+
+
+
