5d222c1a1021a80e4db4db81d38ae31debafff39
Scott Francis <scottrfrancis@mini.local>
2014-12-04 07:08:16 -0800

    update

--- getMaxSustainedHR.R
+++ getMaxSustainedHR.R
@@ -5,6 +5,6 @@ getMaxSustainedHR<- function( time_S.elapsed, heartrate_bpm, window_S )
   
   z<- .xts( heartrate_bpm, time_S.elapsed )
   zz<- na.locf(z)
-  ep<- endpoints(zz, 'seconds', window_S )  ## this isn't quite right yet...
+  ep<- endpoints(zz, 'seconds', window_S/2 )  ## this isn't quite right yet...
   period.apply(zz, ep, min )
 }
