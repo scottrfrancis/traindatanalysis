9f69a0ae6a342bf1697cf40053e5181f82502988
Scott Francis <scottrfrancis@mini.local>
2014-12-08 17:25:13 -0800

    refactoring

--- initMongo.R
+++ initMongo.R
@@ -1,29 +1,18 @@
 library(rmongodb)
-source('summarizeActivitySamplesForUser.R')
+#source('summarizeActivitySamplesForUser.R')
 
-
+initMongo<- function( host = "localhost:27017",
+                      username = "",
+                      password = "",
+                      db = "test" ) 
+{
 # your connection details will likely differ from these defaults
-host <- "quantathlete.com:27017"
-host<- "localhost:27017"
-username <- ""
-password <- ""
-db <- "quantathlete"
 
 #connect to mongo
-#the create function has the following signature
-#mongo.create(host="127.0.0.1", name="", username="", password="", db="admin", timeout=0L)
+  #the create function has the following signature
+  #mongo.create(host="127.0.0.1", name="", username="", password="", db="admin", timeout=0L)
+  
+  mongo <- mongo.create(host=host , db=db, username=username, password=password)
 
-mongo <- mongo.create(host=host , db=db, username=username, password=password)
-
-
-# build a query to find all Users
-
-users<- mongo.find.all( mongo, 'quantathlete.users', fields=mongo.bson.from.list( c(list(`_id`=1L))) )
-uid<- users[[1]]$`_id`
-
-for ( u in users )
-{
-  print( u$`_id` )
-  sapply( c('Swim', 'Ride', 'Run'), function(a) summarizeActivitySamplesForUser(u$`_id`, a) )
+  return( mongo )
 }
-
