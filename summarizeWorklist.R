5039d9636ee672b4cc4b5164bbc3c2ad78d2514f
Scott Francis <scott.russell.francis@gmail.com>
2014-12-22 13:37:22 -0800

    refactoring loop

--- summarizeWorklist.R
+++ summarizeWorklist.R
@@ -1,23 +1,36 @@
-# require('rjson')
+source( 'config.R' )
+
 source( 'initMongo.R' )
 library(rmongodb)
 source('summarizeWorkoutList.R')
 
-mongo<- initMongo( host='quantathlete.com', db='quantathlete')
-#mongo<- initMongo( host='localhost', db='quantathlete')
+mongo<- initMongo( host=dbHost, db=dbName )
 
 
-# read IDs for workouts that need summaries
-# list.filename<- 'worklist.json'
+#workouts.list<- mongo.find.all( mongo, 'quantathlete.workoutSummariesOrphans', fields=mongo.bson.from.list( c(list('workout_id'=1L))) )
 
-# args <- commandArgs(trailingOnly = TRUE)
-# list.filename<- args[0]
+orphans.collection<- paste( dbName, "workoutSummariesOrphans", sep="." )
 
-# list.json<- fromJSON( file= list.filename)
+while( true )
+{
+  workout.bson<- mongo.find.one( mongo, orphans.collection, fields=mongo.bson.from.list( c(list('workout_id'=1L))) ) 
+  if ( is.null( workout.bson ) )
+  {
+    break
+  }
+  # remove from list right away so no oneelse gets it.  if we fail, the bath job will re-enter it...
+  
+  buf<- mongo.bson.buffer.create(); mongo.bson.buffer.append( buf, "workout_id", workout.oid ); q.bson<- mongo.bson.from.buffer( buf )
+  mongo.remove( mongo, 'quantathlete.workoutSummariesOrphans', q.bson )
+  
+  workout.oid<- mongo.bson.to.list( workout.bson )$workout_id
 
-workouts.list<- mongo.find.all( mongo, 'quantathlete.workoutSummariesOrphans', fields=mongo.bson.from.list( c(list('workout_id'=1L))) )
-# workouts.list<- mongo.find.all( mongo, 'quantathlete.workouts', fields=mongo.bson.from.list( c( list( '_id'=1L))))
+  summarizeWorkout( mongo, workout.oid )
+}
+
+
+#workouts.list<- mongo.find.all( mongo, 'quantathlete.workouts', fields=mongo.bson.from.list( c( list( '_id'=1L))))
 # workouts.ids<- lapply( workouts.list, function(w) mongo.oid.from.string(w$'_id' ) )
-workouts.ids<- lapply( workouts.list, function(w) mongo.oid.from.string(w$'workout_id' ) )
+#workouts.ids<- lapply( workouts.list, function(w) mongo.oid.from.string(w$'workout_id' ) )
 
-summarizeWorkoutList( mongo, workouts.ids )
+#summarizeWorkoutList( mongo, workouts.ids )
--- summarizeWorkoutList.R
+++ summarizeWorkoutList.R
@@ -1,6 +1,94 @@
 require('rmongodb')
 source('getMaxSustainedMeasure.R')
 
+summarizeWorkout<- function( mongo, workout.oid )
+{
+  buf<- mongo.bson.buffer.create(); mongo.bson.buffer.append( buf, "workout_id", workout.oid ); q.bson<- mongo.bson.from.buffer( buf )
+  workout.samples<- mongo.find.all( mongo, 'quantathlete.samples', q.bson )
+  
+  if ( length( workout.samples ) <= 0 )
+  {
+    print( "no workouts for user")
+    return()
+  }
+  print( "processing samples")
+  
+  names.use<- names( workout.samples[[1]] )
+  names.use=names.use[(names.use!='latlng')] 
+  names.use<- c( 'latitude', 'longitude', names.use )
+  
+  s<- workout.samples[[1]]
+  
+  #
+  # build data frame from source sample
+  #
+  
+  # idea is blank a dataframe and fill it in...
+  sample.df<- data.frame( matrix( NA, nrow= length( s$time), ncol = length( names.use )))
+  names(sample.df)<- names.use
+  
+  # fix latlng
+  if ( !is.null( s[['latlng']]) )
+  {
+    latlng.matrix<- sapply( s[['latlng']], cbind )
+    s[['latitude']]<- latlng.matrix[1,]
+    s[['longitude']]<- latlng.matrix[2,]
+  }
+  
+  # suppress silly 0's
+  s[['heartrate']][which( s[['heartrate']] == 'NULL' )] <- NA
+  s[['heartrate']][which(s[['heartrate']]==0)]<- NA
+  s[['watts']][which( s[['watts']] == 'NULL' )] <- NA
+  s[['watts']][which(s[['watts']]==0)]<- NA
+  
+  sample.df[,names.use]<- s[names.use]  # copy by names in use    
+  
+  time.windows<- c( 6, 12, 3*60, 6*60, 20*60, 60*60 )
+  
+  # compute pace in m/s
+  if ( !is.null( s[['distance']] ) )
+  {
+    time0<- c( 0, s[['time']][1:(length(s[['time']]) - 1)])
+    dur<- s[['time']] - time0
+    
+    dist0<- c( 0, s[['distance']][1:length(s[['distance']]) - 1])
+    length<- s[['distance']] - dist0
+    
+    speed.mpS<- length/dur
+    
+    #        cat( paste0( "ready to call max with ", length(sample.df$time), " and ", length( speed.mpS ) ) )
+    speed.windows<- sapply( time.windows, function(t) getMaxSustainedMeasure( sample.df$time, speed.mpS, t ) )
+  }
+  
+  heartrate.windows<- sapply( time.windows, function(t) getMaxSustainedMeasure( sample.df$time, unlist(sample.df$heartrate), t ) )
+  watts.windows<- sapply( time.windows, function(t) getMaxSustainedMeasure( sample.df$time, unlist(sample.df$watts), t) )      
+  
+  ## TODO: check if adding NA summaries causes downstream problems
+ # if ( !all( is.na( heartrate.windows ) ) )
+  {
+    ## TODO:  This won't get executed for VERY short workouts...  need to catch those and delete from worklist...
+    
+    #  db.workoutSummaries.update( { 'workout_id': ObjectId( '547a47839cb06a51a04196c1' ) }, {$set: { 'timeWindows': [6,12,180,360,1200,3600], 'heartrateWindows': [177, 176, 169, 168, 165, 146] }},{'upsert':1} )
+    buf<- mongo.bson.buffer.create()
+    mongo.bson.buffer.append( buf, "workout_id", mongo.oid.from.string(s$workout_id) )
+    criteria.bson<- mongo.bson.from.buffer( buf )
+    
+    doc.list<- list( "workout_id"=mongo.oid.from.string(s$workout_id), 
+                     "timeWindows"=time.windows,  
+                     "heartrateWindows"=heartrate.windows,
+                     "wattsWindows"=watts.windows )
+    if ( !is.null( s[['distance']]) )
+    {
+      doc.list$paceWindows<- speed.windows
+    }
+    
+    mongo.update( mongo, 'quantathlete.workoutSummaries', 
+                  criteria.bson,                  
+                  mongo.bson.from.list( doc.list ), 
+                  mongo.update.upsert )
+  }
+}
+
 summarizeWorkoutList<- function( mongo, workouts.ids )
 { 
   i<-1
@@ -9,89 +97,8 @@ summarizeWorkoutList<- function( mongo, workouts.ids )
     # w<- workouts.ids[[1]]
     cat( paste0(i, ".", mongo.oid.to.string(w)) )
     
-    buf<- mongo.bson.buffer.create(); mongo.bson.buffer.append( buf, "workout_id", w ); q.bson<- mongo.bson.from.buffer( buf )
-    workout.samples<- mongo.find.all( mongo, 'quantathlete.samples', q.bson )
+    summarizeWorkout( mongo, w )
     
-    if ( length( workout.samples ) <= 0 )
-    {
-      print( "no workouts for user")
-      return()
-    }
-    print( "processing samples")
-        
-    names.use<- names( workout.samples[[1]] )
-    names.use=names.use[(names.use!='latlng')] 
-    names.use<- c( 'latitude', 'longitude', names.use )
-    
-    for( s in workout.samples )
-    {
-      #  s<- workout.samples[[1]]
-      #
-      # build data frame from source sample
-      #
-      
-      # idea is blank a dataframe and fill it in...
-      sample.df<- data.frame( matrix( NA, nrow= length( s$time), ncol = length( names.use )))
-      names(sample.df)<- names.use
-      
-      # fix latlng
-      if ( !is.null( s[['latlng']]) )
-      {
-        latlng.matrix<- sapply( s[['latlng']], cbind )
-        s[['latitude']]<- latlng.matrix[1,]
-        s[['longitude']]<- latlng.matrix[2,]
-      }
-      
-      # suppress silly 0's
-      s[['heartrate']][which( s[['heartrate']] == 'NULL' )] <- NA
-      s[['heartrate']][which(s[['heartrate']]==0)]<- NA
-      s[['watts']][which( s[['watts']] == 'NULL' )] <- NA
-      s[['watts']][which(s[['watts']]==0)]<- NA
-      
-      sample.df[,names.use]<- s[names.use]  # copy by names in use    
-      
-      time.windows<- c( 6, 12, 3*60, 6*60, 20*60, 60*60 )
-
-      # compute pace in m/s
-      if ( !is.null( s[['distance']] ) )
-      {
-        time0<- c( 0, s[['time']][1:(length(s[['time']]) - 1)])
-        dur<- s[['time']] - time0
-        
-        dist0<- c( 0, s[['distance']][1:length(s[['distance']]) - 1])
-        length<- s[['distance']] - dist0
-        
-        speed.mpS<- length/dur
-        
-#        cat( paste0( "ready to call max with ", length(sample.df$time), " and ", length( speed.mpS ) ) )
-        speed.windows<- sapply( time.windows, function(t) getMaxSustainedMeasure( sample.df$time, speed.mpS, t ) )
-      }
-      
-      heartrate.windows<- sapply( time.windows, function(t) getMaxSustainedMeasure( sample.df$time, unlist(sample.df$heartrate), t ) )
-      watts.windows<- sapply( time.windows, function(t) getMaxSustainedMeasure( sample.df$time, unlist(sample.df$watts), t) )      
-
-      if ( !all( is.na( heartrate.windows ) ) )
-      {
-        #  db.workoutSummaries.update( { 'workout_id': ObjectId( '547a47839cb06a51a04196c1' ) }, {$set: { 'timeWindows': [6,12,180,360,1200,3600], 'heartrateWindows': [177, 176, 169, 168, 165, 146] }},{'upsert':1} )
-        buf<- mongo.bson.buffer.create()
-        mongo.bson.buffer.append( buf, "workout_id", mongo.oid.from.string(s$workout_id) )
-        criteria.bson<- mongo.bson.from.buffer( buf )
-        
-        doc.list<- list( "workout_id"=mongo.oid.from.string(s$workout_id), 
-                          "timeWindows"=time.windows,  
-                          "heartrateWindows"=heartrate.windows,
-                          "wattsWindows"=watts.windows )
-        if ( !is.null( s[['distance']]) )
-        {
-          doc.list$paceWindows<- speed.windows
-        }
-        
-        mongo.update( mongo, 'quantathlete.workoutSummaries', 
-                      criteria.bson,                  
-                      mongo.bson.from.list( doc.list ), 
-                      mongo.update.upsert )
-      }
-    }
   
     # remove this id from the worklist
     
