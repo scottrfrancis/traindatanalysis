5e5a8c4a442b72d5bdcd29bfe7ba57346d8b3d09
scott francis <scott.russell.francis@gmail.com>
2014-12-25 13:35:33 -0800

    changed sub-sampling method to mean from min to better match TP and other analytics.

--- getMaxSustainedMeasure.R
+++ getMaxSustainedMeasure.R
@@ -2,11 +2,11 @@ require('xts')
 
 getMaxSustainedMeasure<- function( time_S.elapsed, metric, window_S )
 {
-  
   z<- .xts( metric, time_S.elapsed )
   zz<- na.locf(z)
   ep<- endpoints(zz, 'seconds', window_S/4 ) 
-  mins<- period.apply(zz, ep, min )
+#  mins<- period.apply(zz, ep, min )
+  means<- period.apply(zz, ep, mean )
   
   ## rebucket the mins in groups of 4 -- See Nyquist and Landau for Sampling theory of regular and irregular intervals
   # that is, take the mins of a SLIDING WINDOW of 4 mins
@@ -17,7 +17,8 @@ getMaxSustainedMeasure<- function( time_S.elapsed, metric, window_S )
     return( NA )
   }
   
-  max.metric<- max( rollapply( coredata(mins)[,1], 4, min ) )
+#  max.metric<- max( rollapply( coredata(mins)[,1], 4, min ) )
+  max.metric<- max( rollapply( coredata(means)[,1], 4, min ) )
   
   return( max.metric )
 }
